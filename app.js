0234c93fae0b1f72ca0c0c84ce68b86117302663
lalaavipsha <53574326+lalaavipsha@users.noreply.github.com>
2022-10-14 20:04:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, Browse, SignIn, SignUp} from './pages';
+import * as ROUTES from './constants/routes';
+export default function App() {
+  return (
+  <Router>
+    <Route exact path="/browse">
+    <Browse />
+  </Route>
+  <Route exact path="/signin">
+    <SignIn />
+  </Route>
+  <Route exact path="/signup">
+    <SignUp />
+  </Route>
+  <Route exact path={ROUTES.HOME}>
+    <Home />
+ 
+  </Route>
+  
+  </Router>
+  );
+}
